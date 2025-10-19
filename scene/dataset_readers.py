2d0026199557be62f2e6f70b0f3e727217b52a71
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-10-19 12:27:48 +0100

    postfix

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -214,9 +214,9 @@ def readColmapSceneInfo_llff(path, images, eval, llffhold, postfix=""):
         reading_dir_train = images_folder
         reading_dir_test = images_folder
 
-    cam_infos_unsorted = readColmapCameras(cam_extrinsics=cam_extrinsics, cam_intrinsics=cam_intrinsics,
+    cam_infos_unsorted_train = readColmapCameras(cam_extrinsics=cam_extrinsics, cam_intrinsics=cam_intrinsics,
                                            images_folder=os.path.join(path, reading_dir_train))
-    cam_infos = sorted(cam_infos_unsorted.copy(), key = lambda x : x.image_name)
+    cam_infos_train = sorted(cam_infos_unsorted_train.copy(), key = lambda x : x.image_name)
     if eval:
         cam_infos_unsorted_test = readColmapCameras(cam_extrinsics=cam_extrinsics, cam_intrinsics=cam_intrinsics, images_folder=os.path.join(path, reading_dir_test))
         cam_infos_test = sorted(cam_infos_unsorted_test.copy(), key = lambda x : x.image_name)
@@ -224,7 +224,7 @@ def readColmapSceneInfo_llff(path, images, eval, llffhold, postfix=""):
         train_cam_infos = [c for i, c in enumerate(cam_infos_train) if i % 8 != 0]
         test_cam_infos = [c for i, c in enumerate(cam_infos_test) if i % 8 == 0]
     else:
-        train_cam_infos = cam_infos
+        train_cam_infos = cam_infos_train
         test_cam_infos = []
 
     nerf_normalization = getNerfppNorm(train_cam_infos)
